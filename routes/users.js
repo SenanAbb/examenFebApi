cdc800c88f65e8c757a062bd58fb39e2c8a8dc28
EloxZ <45355879+EloxZ@users.noreply.github.com>
2022-02-05 16:51:57 +0100

    Cambio

--- routes/users.js
+++ routes/users.js
@@ -8,9 +8,9 @@ module.exports = function (app, gestorBD) {
     if (email == null) email = "";
     let criterio = {
       $and: [
-        { nombre: { $regex: ".*" + name + ".*" } },
-        { apellido: { $regex: ".*" + surname + ".*" } },
-        { email: { $regex: ".*" + email + ".*" } }
+        { nombre: { $regex: ".*" + name + ".*", $options: i } },
+        { apellido: { $regex: ".*" + surname + ".*", $options: i } },
+        { email: { $regex: ".*" + email + ".*", $options: i } }
       ]
     };
     gestorBD.obtenerItem(criterio, "usuarios", function (usuarios) {
@@ -105,60 +105,5 @@ module.exports = function (app, gestorBD) {
         }
       }
     );
-  });
-
-  app.get("/findUser", function (req, res) {
-    let query = req.query.namesurnameuser;
-    let criterio = {
-      $or: [
-        { nombre: { $regex: ".*" + query + ".*" } },
-        { apellido: { $regex: ".*" + query + ".*" } },
-      ],
-    };
-    gestorBD.obtenerItem(criterio, "usuarios", function (usuarios) {
-      if (usuarios == null) {
-        res.send({
-          Error: {
-            status: 500,
-            data: "Se ha producido un error al obtener la lista de usuarios, intentelo de nuevo más tarde",
-          },
-        });
-      } else {
-        res.send({ status: 200, data: { usuarios: usuarios } });
-      }
-    });
-  });
-
-  app.get("/findUserByEmail/:email", function (req, res) {
-    let email = req.params.email;
-    let criterio = { email: email };
-    gestorBD.obtenerItem(criterio, "usuarios", function (usuario) {
-      if (usuario == null) {
-        res.send({
-          Error: {
-            status: 500,
-            data: "Se ha producido un error al obtener el usuario, intentelo de nuevo más tarde o con otro email",
-          },
-        });
-      } else {
-        res.send({ status: 200, data: { usuarios: usuario } });
-      }
-    });
-  });
-
-  app.get("/findUserById/:id", function (req, res) {
-    let criterio = { _id: gestorBD.mongo.ObjectID(req.params.id) };
-    gestorBD.obtenerItem(criterio, "usuarios", function (usuario) {
-      if (usuario == null) {
-        res.send({
-          Error: {
-            status: 500,
-            data: "Se ha producido un error al obtener el usuario, intentelo de nuevo más tarde o con otro email",
-          },
-        });
-      } else {
-        res.send({ status: 200, data: { usuario: usuario } });
-      }
-    });
   });
 };
